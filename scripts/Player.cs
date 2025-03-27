009434d6fe297762d950e6009e104be77b93866d
Carl Wang <cogear@qq.com>
2025-03-28 01:19:27 +0800

    we have a map now

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 200.0f;
+	public const float Speed = 150.0f;
 	public const float JumpVelocity = -300.0f;
 
 	public override void _PhysicsProcess(double delta)
