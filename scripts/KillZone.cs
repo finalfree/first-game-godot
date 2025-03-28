0128152d641881d3f20777437fd0daf913921f81
Carl Wang <cogear@qq.com>
2025-03-29 03:37:26 +0800

    add death effects

--- scripts/KillZone.cs
+++ scripts/KillZone.cs
@@ -5,12 +5,15 @@ public partial class KillZone : Area2D
 {
     void OnBodyEntered(Node2D body)
     {
+        Engine.TimeScale = 0.5;
+        body.GetNode<CollisionShape2D>("CollisionShape2D").QueueFree();
         GD.Print("OnBodyEntered:" + Name);
         GetNode<Timer>("Timer").Start();
     }
 
     void DeathAfterOneSecond()
     {
+        Engine.TimeScale = 1;
         GD.Print("Reload Game Now");
         GetTree().ReloadCurrentScene();
     }
