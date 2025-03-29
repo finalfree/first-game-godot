aab721f951b3e5cf7fab5597aa29e938b8a4c20f
Carl Wang <carl.wang@wisetechglobal.com>
2025-03-29 18:41:32 +0800

    add a animationPlayer to play sound after collecting a coin. Then after 1 second, queue_free coin node

--- scripts/Coin.cs
+++ scripts/Coin.cs
@@ -3,15 +3,17 @@ using Godot;
 public partial class Coin : Area2D
 {
 	GameManager _gameManager;
+	AnimationPlayer _animationPlayer;
 	public override void _Ready()
 	{
 		_gameManager = GetNode<GameManager>("%GameManager");
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	void OnBodyEntered(Node2D body)
 	{
 		GD.Print("OnBodyEntered:" + Name);
 		_gameManager.AddScore();
-		QueueFree();
+		_animationPlayer.Play("collect");
 	}
 }
