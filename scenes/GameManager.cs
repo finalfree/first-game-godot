e315e4139860a3f7d66e2d66efe36abbdd9eaf66
Carl Wang <carl.wang@wisetechglobal.com>
2025-03-29 16:32:07 +0800

    add score panel

---
+++ scenes/GameManager.cs
@@ -0,0 +1,28 @@
+using Godot;    
+
+public partial class GameManager : Node
+{
+    // public static GameManager Instance;
+    //
+    // public override void _EnterTree()
+    // {
+    //     Instance = this;
+    // }
+
+    public int Score => _score;
+    private int _score = 0;
+
+    Label _scoreLabel;
+    public override void _Ready()
+    {
+        _scoreLabel = GetNode<Label>("CanvasLayer/Label");
+        _scoreLabel.Text = $"Score: {_score}";
+    }
+
+    public void AddScore()
+    {
+        _score++;
+        _scoreLabel.Text = $"Score: {_score}";
+        
+    }
+}
--- scripts/Coin.cs
+++ scripts/Coin.cs
@@ -2,9 +2,16 @@ using Godot;
 
 public partial class Coin : Area2D
 {
+	GameManager _gameManager;
+	public override void _Ready()
+	{
+		_gameManager = GetNode<GameManager>("%GameManager");
+	}
+
 	void OnBodyEntered(Node2D body)
 	{
 		GD.Print("OnBodyEntered:" + Name);
+		_gameManager.AddScore();
 		QueueFree();
 	}
 }
